00ade12bf68bac3a839f4fb576a547f084066514
inoveae-git <inove.ae@hotmail.com>
2026-04-05 11:53:07 -0300

    update v0.1
    acrescentou login e melhorou captação audio

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'pesagem-pwa-v1';
-const urlsToCache = ['./','./index.html','./manifest.json'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
-});
+self.addEventListener('fetch',e=>{});
